edace366bf9156f0e941a9cedd479482d138e9e3
wcwt <51042202+wcwt@users.noreply.github.com>
2020-10-21 17:21:18 +0800

    init

--- train_main.py
+++ train_main.py
@@ -1,9 +1,15 @@
 import pickle
 import numpy as np
+import torch
 
 train_file = "../source_file/csci_data/SR-ARE-train/names_onehots.pickle"
 
 with open (train_file,"rb") as f:
     obj = pickle.load(f)
 
-names,vector = obj
+structure,name = obj['onehots'],obj['names']
+
+#print(len(structure))
+#print(len(name))
+
+print(structure[3])
