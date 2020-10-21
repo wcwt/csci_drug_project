7d85f8caeb226f61b22dc7e1062ea85c7c73eb13
wcwt <51042202+wcwt@users.noreply.github.com>
2020-10-21 17:13:39 +0800

    init

--- train_main.py
+++ train_main.py
@@ -1,4 +1,9 @@
 import pickle
 import numpy as np
 
-train_file = "SR-ARE-train\\"
+train_file = "../source_file/csci_data/SR-ARE-train/names_onehots.pickle"
+
+with open (train_file,"rb") as f:
+    obj = pickle.load(f)
+
+names,vector = obj
