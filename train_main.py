b6c18bfea8595e283530edb7cb9996aef05a74b7
wcwt <51042202+wcwt@users.noreply.github.com>
2020-10-31 00:23:11 +0800

    i

--- train_main.py
+++ train_main.py
@@ -1,6 +1,6 @@
-import pickle
-import tensorflow
-import keras
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
 train_file = "../source_file/csci_data/SR-ARE-train/names_onehots.pickle"
