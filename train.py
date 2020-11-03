5b397b3bd87380ad9b7c9d4a110fede811411a85
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-03 13:36:06 +0800

    init

--- train.py
+++ train.py
@@ -1,17 +1,15 @@
+import numpy as np
+import pickle
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import pickle
-
-import numpy as np
-import pickle
 
 path = "../source_file/csci_data/SR-ARE-train/"
 pickle_in = path + "names_onehots.pickle"
 label_in = path + "names_labels.txt"
 
 def dataloader():
-    # load data part
+    # load data from file
     with open (pickle_in,"rb") as f:
         obj = pickle.load(f)
     feature,name = obj['onehots'],obj['names']
@@ -21,7 +19,17 @@ def dataloader():
 
 def main():
     feature,name,toxic_label = dataloader()
-    print(feature[0])
+    # Define Sequential model with 3 layers
+    model = keras.Sequential(
+        [
+            layers.Dense(2, activation="relu", name="layer1"),
+            layers.Dense(3, activation="relu", name="layer2"),
+            layers.Dense(4, name="layer3"),
+        ]
+    )
+    # Call model on a test input
+    x = tf.ones((3, 3))
+    y = model(x)
 
 
 if __name__ == "__main__":
