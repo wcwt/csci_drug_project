9e4e1cc70b82222489e6bcfcd813824aa55cbf33
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-03 21:57:56 +0800

    inint

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
-import numpy as np
-import pickle
+a = 3
 
-path = "../source_file/csci_data/SR-ARE-train/"
-pickle_in = path + "names_onehots.pickle"
-label_in = path + "names_labels.txt"
+b = 100 if a==3 else 5
 
-def dataloader():
-    # load data part
-    with open (pickle_in,"rb") as f:
-        obj = pickle.load(f)
-    feature,name = obj['onehots'],obj['names']
-    label_data = np.loadtxt(label_in,dtype=str,delimiter=',')
-    toxic_label = label_data[:,0] # get {0,1} form name
-    return feature,name,toxic_label
-
-def main():
-    feature,name,toxic_label = dataloader()
-    print(feature[0])
-
-
-if __name__ == "__main__":
-    main()
+print(b)
--- train.py
+++ train.py
@@ -4,12 +4,15 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-path = "../source_file/csci_data/SR-ARE-train/"
-pickle_in = path + "names_onehots.pickle"
-label_in = path + "names_labels.txt"
+train_path = "../source_file/csci_data/SR-ARE-train/"
+test_path = "../source_file/csci_data/SR-ARE-test/"
+
 size = 70*325
 
-def dataloader():
+def dataloader(mode = "train"):
+    path = train_path if mode == "train" else test_path
+    pickle_in = path + "names_onehots.pickle"
+    label_in = path + "names_labels.txt"
     # load data from file
     with open (pickle_in,"rb") as f:
         obj = pickle.load(f)
@@ -18,7 +21,7 @@ def dataloader():
     toxic_label = np.array(label_data[:,1],dtype=int) # get {0,1} form name
     return feature,name,toxic_label
 
-def train(feature,label):
+def train(train_feature,train_label,test_feature,test_label):
     # Define Sequential model with 3 layers
     model = keras.Sequential(
         [
@@ -31,15 +34,15 @@ def train(feature,label):
     )
     model.compile(optimizer = 'adam',loss = 'sparse_categorical_crossentropy',metrics=['accuracy'])
 
-    model.fit(feature,label,epochs=10,shuffle=True)
+    model.fit(train_feature,train_label,epochs=10,shuffle=True)
 
-    test_loss,test_acc = model.evaluate(feature,label,verbose=10)
+    test_loss,test_acc = model.evaluate(test_feature,test_label,verbose=10)
     print(f"loss = {test_loss}, acc = {test_acc}")
 
 def main():
-    feature,name,toxic_label = dataloader()
-    #print(feature)
+    train_feature,train_name,train_toxic_label = dataloader("train")
+    test_feature,test_name,test_toxic_label = dataloader("test")
     train(feature,toxic_label)
 
-#if __name__ == "__main__":
-main()
+if __name__ == "__main__":
+    main()
