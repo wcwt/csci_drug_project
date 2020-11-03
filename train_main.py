745ae7eb8568d0d386ebc7997134a4bf201aa545
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-03 11:56:26 +0800

    init

--- train_main.py
+++ train_main.py
@@ -8,6 +8,13 @@ train_file = "../source_file/csci_data/SR-ARE-train/names_onehots.pickle"
 with open (train_file,"rb") as f:
     obj = pickle.load(f)
 
-structure,name = obj['onehots'],obj['names']
+feature,name = obj['onehots'],obj['names']
 
-print(name)
+batch_size = 32
+
+train_dataset = tf.data.experimental.make_csv_dataset(
+    train_dataset_fp,
+    batch_size,
+    column_names=column_names,
+    label_name=label_name,
+    num_epochs=1)
