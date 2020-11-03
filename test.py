0441af1eafe9e4ab3ed083bd34d6048213b9ec4f
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-03 22:55:15 +0800

    load_modle

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pickle
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 train_path = "../source_file/csci_data/SR-ARE-train/"
 test_path = "../source_file/csci_data/SR-ARE-test/"
-
-size = 70*325
 
 def dataloader(mode = "train"):
     path = train_path if mode == "train" else test_path
@@ -18,7 +19,11 @@ def dataloader(mode = "train"):
     toxic_label = np.array(label_data[:,1],dtype=int) # get {0,1} form name
     return feature,name,toxic_label
 
-train_feature,train_name,train_toxic_label = dataloader("train")
-test_feature,test_name,test_toxic_label = dataloader("test")
+model = create_model()
 
-print(train_toxic_label.sum()/len(train_toxic_label))
+# Restore the weights
+model.load_weights('./')
+
+test_feature,test_label = dataloader("test")
+# Evaluate the model
+loss,acc = model.evaluate(test_feature, test_label, verbose=2)
