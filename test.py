df06b42327b599418e8d132ed09d337a842f5e4e
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-04 00:58:47 +0800

    i

--- test.py
+++ test.py
@@ -41,10 +41,7 @@ model.summary()
 test_feature,test_name,test_toxic_label = dataloader(test_path)
 
 predict = model.predict(test_feature[:3])
-print(predict)
-"""
 
 with open (file_out,"w+") as f:
     for d in predict:
-        f.write(f"{int(d)}\n")
-"""
+        f.write(f"{argmax(d)}\n")
