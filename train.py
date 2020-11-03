93521b9794062a83c7b60bc865f1663959448b3e
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-03 14:23:27 +0800

    init

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from tensorflow.keras import layers
 path = "../source_file/csci_data/SR-ARE-train/"
 pickle_in = path + "names_onehots.pickle"
 label_in = path + "names_labels.txt"
+size = 70*325
 
 def dataloader():
     # load data from file
@@ -14,23 +15,30 @@ def dataloader():
         obj = pickle.load(f)
     feature,name = obj['onehots'],obj['names']
     label_data = np.loadtxt(label_in,dtype=str,delimiter=',')
-    toxic_label = label_data[:,0] # get {0,1} form name
+    toxic_label = np.array(label_data[:,1],dtype=int) # get {0,1} form name
     return feature,name,toxic_label
 
-def main():
-    feature,name,toxic_label = dataloader()
+def train(feature,label):
     # Define Sequential model with 3 layers
     model = keras.Sequential(
         [
-            layers.Dense(2, activation="relu", name="layer1"),
-            layers.Dense(3, activation="relu", name="layer2"),
-            layers.Dense(4, name="layer3"),
+            layers.Flatten(input_shape = (70,325)),
+            layers.Dense(size, activation="relu", name="layer1"),
+            layers.Dense(32, activation="relu",name="layer2"),
+            layers.Dense(2, activation="softmax", name="output"),
         ]
+    # https://www.tensorflow.org/api_docs/python/tf/keras/losses for loss function
     )
-    # Call model on a test input
-    x = tf.ones((3, 3))
-    y = model(x)
+    model.compile(optimizer = 'adam',loss='BinaryCrossentropy',metrics=['accuracy'])
 
+    model.fit(feature,label,epochs=10)
+
+    test_loss,test_acc = model.evaluate(feature,label,verbose=1)
+    print(f"loss = {test_loss}, acc = {test_acc}")
+
+def main():
+    feature,name,toxic_label = dataloader()
+    train(feature,toxic_label)
 
 if __name__ == "__main__":
     main()
